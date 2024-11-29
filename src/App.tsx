eb60242c53a27dea6d317f4b07cbb7a6c53e1d79
EMS-TI <ememail@hotmail.com>
2024-11-29 03:31:33 +0000

    main

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/deposits" element={<Deposits />} />
         <Route path="*" element={<Navigate to="/" replace />} />
