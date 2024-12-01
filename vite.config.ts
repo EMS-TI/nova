d05885d8a00c023fc43416023815c726a22ab1be
EMS-TI <ememail@hotmail.com>
2024-12-01 18:27:42 +0000

    main

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,10 @@
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: './', // Garante que os caminhos sejam tratados de forma relativa
-  build: {
-    outDir: 'dist', // Diretório de saída para o build
-    assetsDir: 'assets', // Diretório para arquivos estáticos
-    rollupOptions: {
-      input: 'index.html', // Certifique-se de que o Vite sabe onde está o ponto de entrada
-    },
+  plugins: [react()],
+  optimizeDeps: {
+    exclude: ['lucide-react'],
   },
 });
